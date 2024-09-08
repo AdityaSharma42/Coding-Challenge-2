c80767f8072737d947dfd63056c003f29cea7003
AdityaSharma42 <adityasharma3@usf.edu>
2024-09-08 15:28:42 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -1,5 +1,13 @@
 //creating variable bill and variable tip that is based on the value of bill
 let bill= 75
-let tip= 50>=bill>=300? bill*0.15: bill*0.2// here the bill is multiplied by 0.15 to get the tip if the amount is between 50 and 300, if not it will be multiplied by 0.2
-console.log ('The bill was',bill,'the tip was', tip, 'and the total value is' bill+tip);
+let tip= bill>300||bill<50? bill*0.2: bill*0.15// here the bill is multiplied by 0.2 to get the tip if the amount is above 300 or less than 50, if not it will be multiplied by 0.15
+console.log ('The bill was',bill,'the tip was', tip, 'and the total value is', bill+tip);
 // line 4 allows me to display the amount, tip and the final bill in a presentable manner
+function calculateTip(bill) {
+    return bill> 300|| bill< 50? bill*0.2: bill*0.15;
+} // the tip is calculated according to the bill in the same way as earlier, however a function is created that allows users to inpit the bill amount
+const billValue= 100;
+let tipAmount= calculateTip(billValue);// this assigns the function to the variable
+
+console.log ('The tip is',tipAmount)
+
